a46ab193a6ee3c64c742b0b3bce6e5db214c2639
Beckham Scadlock <bscadlock02@gmail.com>
2024-02-28 14:27:12 -0500

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -1,13 +1,13 @@
 public class Digits {
     public static void main(String[] args){
     int num;
-    num = 526;
+    num = 258;
     int hundred;
     int ten;
     int one;
-    one = (num % 1);
-    ten = (num % 10);
-    hundred = (num % 100);
+    one = (num % 10);
+    ten = (num % 100)/10;
+    hundred = (num /100);
     System.out.println("The hundreds-place digit is:" + hundred);
     System.out.println("The tens-place digit is:" + ten);
     System.out.println("The ones-place digit is:" + one);
