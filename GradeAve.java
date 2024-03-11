07db83e12f3036f71a66f905661e075e8e9e5cc1
Beckham Scadlock <bscadlock02@gmail.com>
2024-03-11 14:40:19 -0400

    fix

--- GradeAve.java
+++ GradeAve.java
@@ -1,1 +1,27 @@
-public class GradeAve {}
+import java.util.*;
+public class GradeAve {
+    public static void main (String[] args){
+    
+    int grade1;
+    int grade2;
+    int grade3;
+    int grade4;
+    int grade5;
+    int average;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter Your 1st Grade");
+    grade1 = input.nextInt();
+    System.out.println("Enter Your 2nd Grade");
+    grade2 = input.nextInt();
+    System.out.println("Enter Your 3rd Grade");
+    grade3 = input.nextInt();
+    System.out.println("Enter Your 4th Grade");
+    grade4 = input.nextInt();
+    System.out.println("Enter Your 5th Grade");
+    grade5 = input.nextInt();
+    average = (grade1 + grade2 + grade3 + grade4 + grade5)/5;
+    System.out.println("Your Grade Averge is " + average);
+
+}
+}
--- TempConverter.java
+++ TempConverter.java
@@ -1,3 +1,15 @@
-public class TempConverter {
-    
+import java.util.*;
+public class TempConverter 
+{  
+    public static void main (String[] args){
+    int TempF;
+    int TempC;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the Tempurture*F:");
+    TempF = input.nextInt();
+
+    TempC = (TempF - 32)/(9/5);
+    System.out.println("The Tempurture*C Is: " + TempC);
+}
 }
